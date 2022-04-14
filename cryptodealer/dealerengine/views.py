24c87b0267fd579b82d343c25f321e2d519d801a
Krysteins <krysiak.mateusz.94@gmail.com>
2022-04-14 20:02:05 +0200

    bardzo ale to bardzo duzo zrobione

--- cryptodealer/dealerengine/views.py
+++ cryptodealer/dealerengine/views.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.contrib.auth.models import User
@@ -40,19 +41,25 @@ class MarketWeb(View):
     def post(self, request):
         current_user = request.user
         user_id = current_user.id
-        member_buy = Users.objects.filter(pk=user_id)
-        if 'buying0' in request.POST.keys() and request.POST['buying0']:
-            member_buy.member = 0
-        if 'buying1' in request.POST.keys() and request.POST['buying1']:
-            member_buy.member = 1
-        if 'buying2' in request.POST.keys() and request.POST['buying2']:
-            member_buy.member = 2
-        if 'buying3' in request.POST.keys() and request.POST['buying3']:
-            member_buy.member = 3
-        if 'buying4' in request.POST.keys() and request.POST['buying4']:
-            member_buy.member = 4
-        for objects in member_buy:
-            objects.save()
+        member_buy = Users.objects.get(account=user_id)
+        members = Membership.objects.all()
+        money = Users.objects.get(account=user_id)
+        if 'buy0' in request.POST.keys():
+            member_buy.member = members[4]
+        if 'buy1' in request.POST.keys():
+            member_buy.member = members[0]
+            money.usd = money.usd - members[0].price
+        if 'buy2' in request.POST.keys():
+            member_buy.member = members[1]
+            money.usd = money.usd - members[1].price
+        if 'buy3' in request.POST.keys():
+            member_buy.member = members[2]
+            money.usd = money.usd - members[2].price
+        if 'buy4' in request.POST.keys():
+            member_buy.member = members[3]
+            money.usd = money.usd - members[3].price
+        member_buy.save()
+        money.save()
 
         return redirect('market_view')
 
@@ -62,6 +69,9 @@ class ProfileWeb(View):
         current_user = request.user
         user_id = current_user.id
         users = Users.objects.filter(account=user_id)
+        ##########
+        crypto = 1
+        value = 2
         context = {
             'users': users
         }
@@ -74,11 +84,37 @@ class Bitcoin(View):
         current_user = request.user
         user_id = current_user.id
         users = Users.objects.filter(account=user_id)
+        bitcoin = Crypto.objects.filter(pk=1)
+        my_value = Value.objects.filter(account=user_id).filter(crypto=1)
         #######
+
         context = {
-            'users': users
+            'users': users,
+            'bitcoin': bitcoin,
+            'my_value': my_value,
         }
         return render(request, "list_crypto/1.html", context=context)
+
+    def post(self, request):
+        current_user = request.user
+        user_id = current_user.id
+        crypto_price = Crypto.objects.get(name="Bitcoin")
+        my_value = Value.objects.get(account=user_id, crypto=1)
+        money = Users.objects.get(account=user_id)
+        ############################
+        if request.POST['take_price'] != '':
+            take_price = request.POST['take_price']
+            take_prices = Decimal(take_price)
+            if 'buy' in request.POST.keys():
+                my_value.value += take_prices
+                money.usd = money.usd - (crypto_price.price * take_prices)
+            if 'sell' in request.POST.keys():
+                my_value.value -= take_prices
+                money.usd = money.usd + (crypto_price.price * take_prices)
+            money.save()
+            my_value.save()
+
+        return redirect("bitcoin_view")
 
 
 class Litecoin(View):
@@ -86,11 +122,37 @@ class Litecoin(View):
         current_user = request.user
         user_id = current_user.id
         users = Users.objects.filter(account=user_id)
+        litecoin = Crypto.objects.filter(pk=2)
+        my_value = Value.objects.filter(account=user_id).filter(crypto=2)
         #######
+
         context = {
-            'users': users
+            'users': users,
+            'litecoin': litecoin,
+            'my_value': my_value,
         }
         return render(request, "list_crypto/2.html", context=context)
+
+    def post(self, request):
+        current_user = request.user
+        user_id = current_user.id
+        crypto_price = Crypto.objects.get(name="Litecoin")
+        my_value = Value.objects.get(account=user_id, crypto=2)
+        money = Users.objects.get(account=user_id)
+        ############################
+        if request.POST['take_price'] != '':
+            take_price = request.POST['take_price']
+            take_prices = Decimal(take_price)
+            if 'buy' in request.POST.keys():
+                my_value.value += take_prices
+                money.usd = money.usd - (crypto_price.price * take_prices)
+            if 'sell' in request.POST.keys():
+                my_value.value -= take_prices
+                money.usd = money.usd + (crypto_price.price * take_prices)
+            money.save()
+            my_value.save()
+
+        return redirect("litecoin_view")
 
 
 class Dogecoin(View):
@@ -98,11 +160,37 @@ class Dogecoin(View):
         current_user = request.user
         user_id = current_user.id
         users = Users.objects.filter(account=user_id)
+        dogecoin = Crypto.objects.filter(pk=3)
+        my_value = Value.objects.filter(account=user_id).filter(crypto=3)
         #######
+
         context = {
-            'users': users
+            'users': users,
+            'dogecoin': dogecoin,
+            'my_value': my_value,
         }
         return render(request, "list_crypto/3.html", context=context)
+
+    def post(self, request):
+        current_user = request.user
+        user_id = current_user.id
+        crypto_price = Crypto.objects.get(name="Dogecoin")
+        my_value = Value.objects.get(account=user_id, crypto=3)
+        money = Users.objects.get(account=user_id)
+        ############################
+        if request.POST['take_price'] != '':
+            take_price = request.POST['take_price']
+            take_prices = Decimal(take_price)
+            if 'buy' in request.POST.keys():
+                my_value.value += take_prices
+                money.usd = money.usd - (crypto_price.price * take_prices)
+            if 'sell' in request.POST.keys():
+                my_value.value -= take_prices
+                money.usd = money.usd + (crypto_price.price * take_prices)
+            money.save()
+            my_value.save()
+
+        return redirect("dogecoin_view")
 
 
 class Tether(View):
@@ -110,11 +198,37 @@ class Tether(View):
         current_user = request.user
         user_id = current_user.id
         users = Users.objects.filter(account=user_id)
+        tether = Crypto.objects.filter(pk=4)
+        my_value = Value.objects.filter(account=user_id).filter(crypto=4)
         #######
+
         context = {
-            'users': users
+            'users': users,
+            'tether': tether,
+            'my_value': my_value,
         }
         return render(request, "list_crypto/4.html", context=context)
+
+    def post(self, request):
+        current_user = request.user
+        user_id = current_user.id
+        crypto_price = Crypto.objects.get(name="Tether")
+        my_value = Value.objects.get(account=user_id, crypto=4)
+        money = Users.objects.get(account=user_id)
+        ############################
+        if request.POST['take_price'] != '':
+            take_price = request.POST['take_price']
+            take_prices = Decimal(take_price)
+            if 'buy' in request.POST.keys():
+                my_value.value += take_prices
+                money.usd = money.usd - (crypto_price.price * take_prices)
+            if 'sell' in request.POST.keys():
+                my_value.value -= take_prices
+                money.usd = money.usd + (crypto_price.price * take_prices)
+            money.save()
+            my_value.save()
+
+        return redirect("tether_view")
 
 
 class Ethereum(View):
@@ -122,8 +236,34 @@ class Ethereum(View):
         current_user = request.user
         user_id = current_user.id
         users = Users.objects.filter(account=user_id)
+        ethereum = Crypto.objects.filter(pk=5)
+        my_value = Value.objects.filter(account=user_id).filter(crypto=5)
         #######
+
         context = {
-            'users': users
+            'users': users,
+            'ethereum': ethereum,
+            'my_value': my_value,
         }
         return render(request, "list_crypto/5.html", context=context)
+
+    def post(self, request):
+        current_user = request.user
+        user_id = current_user.id
+        crypto_price = Crypto.objects.get(name="Ethereum")
+        my_value = Value.objects.get(account=user_id, crypto=5)
+        money = Users.objects.get(account=user_id)
+        ############################
+        if request.POST['take_price'] != '':
+            take_price = request.POST['take_price']
+            take_prices = Decimal(take_price)
+            if 'buy' in request.POST.keys():
+                my_value.value += take_prices
+                money.usd = money.usd - (crypto_price.price * take_prices)
+            if 'sell' in request.POST.keys():
+                my_value.value -= take_prices
+                money.usd = money.usd + (crypto_price.price * take_prices)
+            money.save()
+            my_value.save()
+
+        return redirect("ethereum_view")
