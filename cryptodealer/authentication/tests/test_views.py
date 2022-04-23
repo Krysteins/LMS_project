2aa98e34152d916141134a341f5f80519f5e9b40
Krysteins <krysiak.mateusz.94@gmail.com>
2022-04-23 12:43:51 +0200

    dupa

--- cryptodealer/authentication/tests/test_views.py
+++ cryptodealer/authentication/tests/test_views.py
@@ -7,61 +7,61 @@ from dealerengine import models, views
 from django.urls import reverse
 from django.test import Client
 from authentication import views
-#
-#
-# @pytest.mark.parametrize('param', [
-#     ('home'),
-#     ('register_view'),
-#     ('login_view')
-# ])
-# def test_render_views(client, param):
-#     temp_url = urls.reverse(param)
-#     resp = client.get(temp_url)
-#     assert resp.status_code == 200
-#
-#
-# @pytest.mark.django_db
-# def test_user_signup(client, user_data):
-#     user_model = get_user_model()
-#     register_urls = urls.reverse('register_view')
-#     print(user_model.objects.all())
-#     print(user_model.objects.count())
-#     resp = client.post(register_urls, user_data)
-#     print(user_model.objects.all())
-#     print(user_model.objects.count())
-#     assert resp.status_code == 302
-#
-#
-#
-# @pytest.mark.django_db
-# def test_user_login(client, create_test_user, user_data):
-#     user_model = get_user_model()
-#     print(user_model.objects.count())
-#     login_url = urls.reverse('login_view')
-#     resp = client.post(login_url, data=user_data)
-#     assert resp.status_code == 302
-#     assert resp.url == urls.reverse('login_view')
-#
 
-#
-#
-# @pytest.mark.django_db
-# def test_user_logout(client, authenticated_user):
-#     logout_url = urls.reverse('signout')
-#     resp = client.get(logout_url)
-#     assert resp.status_code == 302
-#     assert resp.url == urls.reverse('home')
-#
-#
-# def test_new_user(django_user_model):
-#     user_model = get_user_model()
-#     print(user_model.objects.count())
-#     new_account = django_user_model.objects.create(username="username1", password="password1")
-#     print(user_model.objects.count())
-#     new_account.save()
-#     print(new_account)
-#
-#
-# def test_an_admin_view(admin_client):
-#     response = admin_client.get('/admin/')
-#     assert response.status_code == 200
+
+@pytest.mark.parametrize('param', [
+    ('home'),
+    ('register_view'),
+    ('login_view')
+])
+def test_render_views(client, param):
+    temp_url = urls.reverse(param)
+    resp = client.get(temp_url)
+    assert resp.status_code == 200
+
+
+@pytest.mark.django_db
+def test_user_signup(client, user_data):
+    user_model = get_user_model()
+    register_urls = urls.reverse('register_view')
+    print(user_model.objects.all())
+    print(user_model.objects.count())
+    resp = client.post(register_urls, user_data)
+    print(user_model.objects.all())
+    print(user_model.objects.count())
+    assert resp.status_code == 302
+
+
+
+@pytest.mark.django_db
+def test_user_login(client, create_test_user, user_data):
+    user_model = get_user_model()
+    print(user_model.objects.count())
+    login_url = urls.reverse('login_view')
+    resp = client.post(login_url, data=user_data)
+    assert resp.status_code == 302
+    assert resp.url == urls.reverse('login_view')
+
+
+
+
+@pytest.mark.django_db
+def test_user_logout(client, authenticated_user):
+    logout_url = urls.reverse('signout')
+    resp = client.get(logout_url)
+    assert resp.status_code == 302
+    assert resp.url == urls.reverse('home')
+
+
+def test_new_user(django_user_model):
+    user_model = get_user_model()
+    print(user_model.objects.count())
+    new_account = django_user_model.objects.create(username="username1", password="password1")
+    print(user_model.objects.count())
+    new_account.save()
+    print(new_account)
+
+
+def test_an_admin_view(admin_client):
+    response = admin_client.get('/admin/')
+    assert response.status_code == 200
--- cryptodealer/dealerengine/tests/test_views.py
+++ cryptodealer/dealerengine/tests/test_views.py
@@ -1,44 +1,47 @@
+from decimal import Decimal
+
 from django import urls
 from django.contrib.auth import get_user_model
 import pytest
 
-#
-# # test with logged user and try open website
-# @pytest.mark.parametrize('param', [
-#     ('profile_view'),
-#     ('crypto_view'),
-#     ('market_view'),
-#     ('history_view'),
-#     ('bitcoin_view'),
-#     ('litecoin_view'),
-#     ('dogecoin_view'),
-#     ('tether_view'),
-#     ('ethereum_view'),
-# ])
-# @pytest.mark.django_db
-# def test_render_views_with_login(client, authenticated_user, param):
-#     temp_url = urls.reverse(param)
-#     resp = client.get(temp_url)
-#     print(temp_url)
-#     assert resp.status_code == 200
-#
-#
-# # test with not logged user and try open website
-# @pytest.mark.parametrize('param', [
-#     ('profile_view'),
-#     ('crypto_view'),
-#     ('market_view'),
-#     ('history_view'),
-#     ('bitcoin_view'),
-#     ('litecoin_view'),
-#     ('dogecoin_view'),
-#     ('tether_view'),
-#     ('ethereum_view'),
-# ])
-# def test_render_views(client, param):
-#     temp_url = urls.reverse(param)
-#     resp = client.get(temp_url)
-#     assert resp.status_code == 302
+
+# test with logged user and try open website
+@pytest.mark.parametrize('param', [
+    ('profile_view'),
+    ('crypto_view'),
+    ('market_view'),
+    ('history_view'),
+    ('bitcoin_view'),
+    ('litecoin_view'),
+    ('dogecoin_view'),
+    ('tether_view'),
+    ('ethereum_view'),
+])
+@pytest.mark.django_db
+def test_render_views_with_login(client, authenticated_user, param):
+    temp_url = urls.reverse(param)
+    resp = client.get(temp_url)
+    print(temp_url)
+    assert resp.status_code == 200
+
+
+# test with not logged user and try open website
+@pytest.mark.parametrize('param', [
+    ('profile_view'),
+    ('crypto_view'),
+    ('market_view'),
+    ('history_view'),
+    ('bitcoin_view'),
+    ('litecoin_view'),
+    ('dogecoin_view'),
+    ('tether_view'),
+    ('ethereum_view'),
+    ('add_balance_view'),
+])
+def test_render_views(client, param):
+    temp_url = urls.reverse(param)
+    resp = client.get(temp_url)
+    assert resp.status_code == 302
 
 from django.contrib.auth.models import User
 from dealerengine.models import Users, Value, Membership
@@ -53,8 +56,31 @@ def test_memberships(client, authenticated_user):
     members = Users.objects.get(account=authenticated_user)
     print(members.member)
     assert resp.status_code == 302
-    print(resp.content)
 
+
+@pytest.mark.django_db
+def test_add_balance(client, authenticated_user):
+    balance_url = urls.reverse('add_balance_view')
+    cash = Users.objects.get(account=authenticated_user)
+    monej = 99999999.00
+    assert cash.usd == monej
+    resp = client.post(balance_url, {'cash': 10, 'buy': True})
+    cash = Users.objects.get(account=authenticated_user)
+    assert cash.usd == monej + 10
+
+
+@pytest.mark.django_db
+def test_add_balance_decimal(client, authenticated_user):
+    balance_url = urls.reverse('add_balance_view')
+    cash = Users.objects.get(account=authenticated_user)
+    monej = 99999999.00
+    assert cash.usd == monej
+    resp = client.post(balance_url, {'cash': 10.11, 'buy': True})
+    cash = Users.objects.get(account=authenticated_user)
+    summary1 = Decimal(monej + 10.12)
+    summary2 = Decimal(monej + 10.10)
+    assert cash.usd < summary1
+    assert cash.usd > summary2
 
 ################################################################################## cryptocurrency tests!
 @pytest.mark.django_db
@@ -215,6 +241,7 @@ def test_crypto_dogecoin_add(client, authenticated_user):
     resp = client.post(bitcoin_url, data=data)
     assert resp.status_code == 302
     cryptocurrency = Value.objects.get(account=authenticated_user, crypto=5)
+    assert cryptocurrency.value > 50
     print(cryptocurrency.value)
 
 
@@ -231,4 +258,5 @@ def test_crypto_dogecoin_minus(client, authenticated_user):
     resp = client.post(bitcoin_url, data=data)
     assert resp.status_code == 302
     cryptocurrency = Value.objects.get(account=authenticated_user, crypto=5)
+    assert cryptocurrency.value < 50
     print(cryptocurrency.value)
